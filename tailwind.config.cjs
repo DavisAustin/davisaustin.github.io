0b75fba76d963e5ef18ab572189fcbc9c6663568
Austin Davis <Austin.DavisDesigns@Gmail.Com>
2023-12-19 10:28:08 -0800

    Reinstalled modules and updated Astro

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,7 +3,8 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		fontFamily: {
-			'mono': ['"PT Mono"', 'sans-serif'],
+			'sans': ['"Inter"', 'sans-serif'],
+			'mono': ['"PT Mono"', 'monospace'],
 		},
 		extend: {},
 	},
