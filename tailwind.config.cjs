59f6a8e68bc8175b4e8da98c3fb45f9dd46984dc
Austin Davis <Austin.DavisDesigns@Gmail.Com>
2024-03-31 10:15:22 -0700

    Updated Astro and added tokens

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -10,21 +10,32 @@ module.exports = {
 		},
 		extend: {
 			colors: {
+				// foreground colors
 				'fg-default': 'var(--color-fg-default)',
 				'fg-muted': 'var(--color-fg-muted)',
 				'fg-subtle': 'var(--color-fg-subtle)',
-				'fg-onEmphasis': 'var(--color-fg-onEmphasis)',
 				'fg-accent': 'var(--color-fg-accent)',
-				// Background colors
+				'fg-accent-hover': 'var(--color-fg-accent-hover)',
+				'fg-onEmphasis': 'var(--color-fg-onEmphasis)',
+				// background colors
 				'bg-default': 'var(--color-bg-default)',
+				'bg-default-hover': 'var(--color-bg-default-hover)',
 				'bg-muted': 'var(--color-bg-muted)',
 				'bg-subtle': 'var(--color-bg-subtle)',
+				'bg-subtle-hover': 'var(--color-bg-subtle-hover)',
+				'bg-accent': 'var(--color-bg-accent)',
+				'bg-accent-hover': 'var(--color-bg-accent-hover)',
 				'bg-emphasis': 'var(--color-bg-emphasis)',
-				'bg-gradient': 'var(--color-bg-gradient)',
-				// Border colors
+				'bg-emphasis-hover': 'var(--color-bg-emphasis-hover)',
+				'bg-accent-emphasis': 'var(--color-bg-accent-emphasis)',
+				'bg-accent-emphasis-hover': 'var(--color-bg-accent-emphasis-hover)',
+
+				// border colors
 				'border-default': 'var(--color-border-default)',
 				'border-muted': 'var(--color-border-muted)',
+				'border-accent': 'var(--color-border-accent)',
 				'border-emphasis': 'var(--color-border-emphasis)',
+				'border-accent-emphasis': 'var(--color-border-accent-emphasis)',
 			},
 		},
 	},
