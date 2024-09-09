b4afb7ea71de6b2437b8cbaa9a4d578c6e0fe5de
Austin Davis <Austin.DavisDesigns@Gmail.Com>
2024-09-08 23:05:21 -0700

    Restarting

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,7 @@ module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		fontFamily: {
+			'serif': ['ui-serif', 'Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
 			'sans': ['"Inter"', 'sans-serif'],
 			'mono': ['"PT Mono"', 'monospace'],
 		},
